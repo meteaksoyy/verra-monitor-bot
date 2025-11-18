91dc97fa77473e355a2457b65836c1ac52e7ba0c
meteaksoyy <meteaksoy531@gmail.com>
2025-11-18 23:06:22 +0100

    Debug verra_bot.py

--- verra_bot.py
+++ verra_bot.py
@@ -12,7 +12,7 @@ def fetch_ids():
   url = "https://www.verra.nl/en/realtime-listings/consumer"
   data = requests.get(url).json
   print("DEBUG JSON:")
-  print(json.dumps(data[:2], indent = 2)[:2000])
+  print(json.dumps(data, indent = 2)[:2000])
   return []
 
 def notify(msg):
